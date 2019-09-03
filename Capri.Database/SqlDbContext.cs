1c97fa4d80a6a0c50f802c8bc15f50ff41906cea
Krzysztof Michalak <krzysztof.michalak96@gmail.com>
2019-09-03 22:44:00 +0200

    Add SqlDbContext

---
+++ Capri.Database/SqlDbContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Capri.Database.Entities;
+
+namespace Capri.Database
+{
+    public class SqlDbContext : DbContext, ISqlDbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Promoter> Promoters { get; set; }
+        public DbSet<DeanEmployee> DeanEmployees { get; set; }
+        public DbSet<BachelorProposal> BachelorProposals { get; set; }
+        public DbSet<MasterProposal> MasterProposals { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Promoter>()
+                .HasMany(p => p.Proposals)
+                .WithOne();
+
+            modelBuilder.Entity<Proposal>()
+                .HasMany(p => p.AssignedStudents)
+                .WithOne();
+
+            modelBuilder.Entity<Proposal>()
+                .HasMany(p => p.WillingCandidates)
+                .WithOne();
+        }
+    }
+}
