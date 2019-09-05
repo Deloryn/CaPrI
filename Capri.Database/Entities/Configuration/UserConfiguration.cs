67326f7d601d73b2d1c39ee5fac5b4ea5d06a26d
Krzysztof Michalak <krzysztof.michalak96@gmail.com>
2019-09-05 23:49:51 +0200

    Add UserConfiguration

---
+++ Capri.Database/Entities/Configuration/UserConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.AspNetCore.Identity;
+using Capri.Database.Entities.Identity;
+
+namespace Capri.Database.Entities.Configuration
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UserConfiguration(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async void Configure(EntityTypeBuilder<User> builder)
+        {
+            string email = "admin@gmail.com";
+            string password = "qwerty";
+            var user = new User { UserName = email, Email = email };
+            await _userManager.CreateAsync(user, password);
+        }
+    }
+}
