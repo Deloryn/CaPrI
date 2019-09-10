241119d8792de31655893c808bcb6de0a1a25239
Krzysztof Michalak <krzysztof.michalak96@gmail.com>
2019-09-10 21:34:12 +0200

    Add Primary Key for GuidUserLogin

--- Capri.Database/Entities/Identity/GuidUserLogin.cs
+++ Capri.Database/Entities/Identity/GuidUserLogin.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 using Microsoft.AspNetCore.Identity;
 
@@ -7,5 +8,7 @@ namespace Capri.Database.Entities.Identity
 {
     public class GuidUserLogin : IdentityUserLogin<Guid>
     {
+        [Key]
+        public Guid Id { get; set; }
     }
 }
