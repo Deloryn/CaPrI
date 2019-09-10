69fbaa922f85fede02231928451525c7a7e688a2
Krzysztof Michalak <krzysztof.michalak96@gmail.com>
2019-09-10 23:29:00 +0200

    Refactor AccountController

--- Capri.Web/Controllers/AccountController.cs
+++ Capri.Web/Controllers/AccountController.cs
@@ -19,26 +19,21 @@ namespace Capri.Web.Controllers
             _accountService = accountService;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
         [AllowAnonymous]
         [HttpPost]
         public async Task<IActionResult> Login([FromBody]UserCredentials credentials)
         {
             if (credentials == null)
                 return BadRequest("Credentials not given");
-            var userToken = await _accountService.Authenticate(credentials.Email, credentials.Password);
+            var userSecurityStamp = await _accountService.Authenticate(credentials.Email, credentials.Password);
            
-            if (userToken == null)
+            if (userSecurityStamp == null)
             {
                 return BadRequest(new { message = "Username or password is incorrect" });
             }
             else
             {
-                return Ok(userToken);
+                return Ok(userSecurityStamp);
             }
         }
     }
