48a47ae9de6ebbb8241a8652e36f7c1b6cefa26e
voxes96 <liszkaszymon@gmail.com>
2019-10-13 16:58:04 +0200

    Separate store into modules. Clean version

--- Capri.Web/src/app.vue
+++ Capri.Web/src/app.vue
@@ -2,21 +2,15 @@
   <div id="app">
     <h1 class="test">Vue works!</h1>
     <Test></Test>
-    <Counter></Counter>
-    <Inc></Inc>
   </div>
 </template>
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
 import Test from '@src/test.vue';
-import Counter from '@src/counter.vue';
-import Inc from '@src/inc.vue';
 
 @Component({
   components: {
-    Test,
-    Counter,
-    Inc,
+    Test
   }
 })
 export default class App extends Vue {
--- Capri.Web/src/store/modules/exampleModule.ts
+++ Capri.Web/src/store/modules/exampleModule.ts
@@ -5,23 +5,13 @@ import Vuex from 'vuex';
 //remove it or replace with your own data
 export const ExampleModule = {
     state: {
-        count: 5,
+
     },
     mutations: {    //synchronous change
-        increment(state) {
-            state.count += 1;
-        },
-        decrement(state) {
-            state.count -= 1;
-        },
+
     },
     actions: {      //asynchronous change
-        increment(context) {
-            context.commit('increment');
-        },
-        decrement(context) {
-            context.commit('decrement');
-        }
+
     },
     getters: {
 
