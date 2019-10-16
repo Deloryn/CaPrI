24e878a240d3700c5fdbf35a3d5844a8a6b9db45
Ginna <DLXPEIN@emea.vwg>
2019-10-16 02:17:55 +0200

    Added test comp

---
+++ Capri.Web/src/test2.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1 class="test1">Test Comp 2</h1>
+  </div>
+</template>
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+
+@Component
+export default class Test2 extends Vue {
+
+}
+</script>
+<style lang="scss" scoped>
+.test1 {
+  color: #F00;
+}
+</style>
+
