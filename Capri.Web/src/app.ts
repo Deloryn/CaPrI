1129a952ee82a79254b2e470df36d95676af0113
Krzyk <krzykarc@gmail.com>
2019-11-13 01:28:35 +0100

    adding store

--- Capri.Web/src/app.ts
+++ Capri.Web/src/app.ts
@@ -1,6 +1,7 @@
 import App from '@src/app.vue';
 import { Vue } from 'vue-property-decorator';
 import Vuetify from 'vuetify';
+import {store} from '@src/store/store.ts';
 import 'vuetify/dist/vuetify.min.css';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import '@mdi/font/css/materialdesignicons.css';
