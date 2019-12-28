9a6d8f2da51fa7cab3e6da5e21b2336dca966af2
Krzysztof Michalak <krzysztof.michalak96@gmail.com>
2019-12-28 22:34:21 +0100

    Correct promoter's fullname formating for .csv file

--- Capri.Web/Configuration/Mapper/ProposalMappingProfile.cs
+++ Capri.Web/Configuration/Mapper/ProposalMappingProfile.cs
@@ -17,11 +17,24 @@ namespace Capri.Web.Configuration.Mapper
                 o=>o.MapFrom(proposal=>proposal.Students.Select(s=>s.Id)));
 
             CreateMap<Proposal, ProposalCsv>()
-            .ForMember(csv => csv.Promoter, o => o.MapFrom(p =>
-                $"{p.Promoter.TitlePrefix} {p.Promoter.FirstName} {p.Promoter.LastName}{", " + p.Promoter.TitlePostfix ?? ""}"))
+            .ForMember(csv => csv.Promoter, o => o.MapFrom(p => GetFullNameFor(p.Promoter)))
             .ForMember(csv => csv.Course, o => o.MapFrom(p => p.Course.Name))
             .ForMember(csv => csv.Faculty, o => o.MapFrom(p => p.Course.Faculty.Name))
             .ForMember(csv => csv.Institute, o => o.MapFrom(p => p.Promoter.Institute.Name));
+        }
+
+        private string GetFullNameFor(Promoter promoter)
+        {
+            var fullName = $"{promoter.TitlePrefix} {promoter.FirstName} {promoter.LastName}";
+            if(promoter.TitlePostfix == null)
+            {
+                return fullName;
+            }
+            else if(promoter.TitlePostfix == string.Empty)
+            {
+                return fullName;
+            }
+            return $"{fullName}, {promoter.TitlePostfix}";
         }
     }
 }
