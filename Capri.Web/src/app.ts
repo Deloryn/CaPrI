e0bed89bc99125063a01cb42192a26bc9fe7ea67
Krzyk <krzykarc@gmail.com>
2020-01-17 20:00:29 +0100

    removing empty object from component and changing props

--- Capri.Web/src/app.ts
+++ Capri.Web/src/app.ts
@@ -3,7 +3,7 @@ import { Vue } from 'vue-property-decorator';
 import Vuetify from 'vuetify';
 import { store } from '@src/store/store.ts';
 import 'vuetify/dist/vuetify.min.css';
-import '@mdi/font/css/materialdesignicons.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import router from './router';
 
 if (module.hot) {
