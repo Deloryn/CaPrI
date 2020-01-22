5d7e1fd00c8c65b94d25fc3c9febad8dfc254056
Krzysztof Michalak <krzysztof.michalak96@gmail.com>
2020-01-22 02:52:48 +0100

    Update layout's proposalService

--- Capri.Web/src/services/proposalService.ts
+++ Capri.Web/src/services/proposalService.ts
@@ -5,6 +5,22 @@ class ProposalService extends CrudService {
     constructor() {
         super("/proposals")
     }
+
+    public getFiltered(params: string) {
+        return requestService.request("GET", "/proposals/filtered?" + params)
+    }
+
+    public getCsv(id: string) {
+        return requestService.request("GET", "/proposals/" + id + "/csv")
+    }
+
+    public calculateSubmittedBachelorProposals(promoterId: string) {
+        return requestService.request("GET", "/proposals/submitted/bachelor/" + promoterId)
+    }
+
+    public calculateSubmittedMasterProposals(promoterId: string) {
+        return requestService.request("GET", "/proposals/submitted/master/" + promoterId)
+    }
 }
 
 export const proposalService = new ProposalService();
