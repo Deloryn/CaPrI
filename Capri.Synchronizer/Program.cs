d6a3e17fcbf76b919c226b1688576825d0510259
Krzysztof Michalak <krzysztof.michalak96@gmail.com>
2020-02-01 13:59:41 +0100

    Create Capri.Synchronizer project

---
+++ Capri.Synchronizer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Capri.Database;
+
+namespace Capri.Synchronizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string capriWebDir = Directory.
+                GetParent(Directory.GetCurrentDirectory())
+                .FullName;
+
+            var connectionString = "???";
+            var optionsBuilder = 
+                new DbContextOptionsBuilder<CapriDbContext>()
+                    .UseSqlServer(connectionString);
+            var context = new CapriDbContext(optionsBuilder.Options);   
+        }
+    }
+}
