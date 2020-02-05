2144d2b12053fc08a821695c9ea68432c297da7f
Krzysztof Michalak <krzysztof.michalak96@gmail.com>
2020-02-05 10:50:26 +0100

    And bachelor and master finish date to SystemSettings

--- Capri.Services/Settings/SystemSettings.cs
+++ Capri.Services/Settings/SystemSettings.cs
@@ -5,6 +5,8 @@ namespace Capri.Services.Settings
     public class SystemSettings
     {
         public DateTime BachelorThesisStartDate { get; set; }
+        public DateTime BachelorThesisFinishDate { get; set; }
         public DateTime MasterThesisStartDate { get; set; }
+        public DateTime MasterThesisFinishDate { get; set; }
     }
 }
